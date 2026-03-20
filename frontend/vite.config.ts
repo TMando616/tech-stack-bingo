fdf2f6dc79e21490907267b47d065b9cc738fab9
TMando616 <t.mandokoro616@gmail.com>
2026-03-20 14:41:51 +0900

    [frontend] html2canvas のインポートエラーを修正するために Vite の事前バンドル設定を追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     optimizeDeps: {
-      include: ['vue-toastification']
+      include: ['vue-toastification', 'html2canvas']
     },
     server: {
       host: '0.0.0.0',
